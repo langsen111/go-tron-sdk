391eaf19397c7467b8800c7d0e633333b4cbf366
langsen <aiyslm@foxmail.com>
2023-05-09 13:26:05 +0800

    del cmd account

--- pkg/keystore/account.go
+++ pkg/keystore/account.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"github.com/langsen111/go-tron-sdk/pkg/address"
 	"github.com/langsen111/go-tron-sdk/pkg/common"
 	"github.com/langsen111/go-tron-sdk/pkg/proto/core"
@@ -173,23 +172,4 @@ func UnmarshalPublic(pbk []byte) (*ecdsa.PublicKey, error) {
 	}
 
 	return &ecdsa.PublicKey{Curve: crypto.S256(), X: x, Y: y}, nil
-}
-
-func RecoverPubkey(hash []byte, signature []byte) (address.Address, error) {
-
-	if signature[64] >= 27 {
-		signature[64] -= 27
-	}
-
-	sigPublicKey, err := secp256k1.RecoverPubkey(hash, signature)
-	if err != nil {
-		return nil, err
-	}
-	pubKey, err := UnmarshalPublic(sigPublicKey)
-	if err != nil {
-		return nil, err
-	}
-
-	addr := address.PubkeyToAddress(*pubKey)
-	return addr, nil
 }
